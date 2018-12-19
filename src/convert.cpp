62195be0bf2c72ce3a7e2c98da90771df721db40
Mike Walton <mikewaltonweb@gmail.com>
2018-12-18 20:09:10 -0600

    Add additional color conversion functions

--- src/convert.cpp
+++ src/convert.cpp
@@ -11,16 +11,16 @@ RgbColor Convert::AnalogToColor(int analogValue) {
     {                  
         analogValue = (analogValue * 3) / 4; // Normalize to 0-255
 
-        redVal = 256 - analogValue;  // Red from full to off
+        redVal = 255 - analogValue;  // Red from full to off
         grnVal = analogValue;        // Green from off to full
-        bluVal = 1;             // Blue off
+        bluVal = 0;             // Blue off
     }
     else if (analogValue < 682) // Middle third of potentiometer's range (341-681)
     {
         analogValue = ( (analogValue-341) * 3) / 4; // Normalize to 0-255
 
-        redVal = 1;            // Red off
-        grnVal = 256 - analogValue; // Green from full to off
+        redVal = 0;            // Red off
+        grnVal = 255 - analogValue; // Green from full to off
         bluVal = analogValue;       // Blue from off to full
     }
     else  // Upper third of potentiometer"s range (682-1023)
@@ -28,8 +28,8 @@ RgbColor Convert::AnalogToColor(int analogValue) {
         analogValue = ( (analogValue-683) * 3) / 4; // Normalize to 0-255
 
         redVal = analogValue;       // Red from off to full
-        grnVal = 1;            // Green off
-        bluVal = 256 - analogValue; // Blue from full to off
+        grnVal = 0;            // Green off
+        bluVal = 255 - analogValue; // Blue from full to off
     }
 
     return RgbColor(redVal, grnVal, bluVal);
@@ -42,6 +42,46 @@ RgbColor* Convert::AnalogRangeToColors(int startValue, int endValue, int stepCou
     for(int i = 0; i < stepCount; i++) {
         colors[i] = AnalogToColor(startValue + round(stepAmount * i));
     }
+
+    return colors;
+}
+
+int Convert::ColorToAnalog(RgbColor color) {
+    if (color.R == 0) {
+        return color.B * 4 / 3 + 341;
+    }
+    else if (color.G == 0) {
+        return color.R  * 4 / 3 + 683;
+    }
+    else if (color.B == 0) {
+        return color.G * 4 / 3;
+    }
+
+    return 0;
+}
+
+int* Convert::ColorRangeToAnalogs(RgbColor startColor, RgbColor endColor, int stepCount) {
+    int* values = new int[stepCount];
+
+    int startValue = ColorToAnalog(startColor);
+    int endValue = ColorToAnalog(endColor);
+    double stepAmount = (endValue - startValue) / (double)(stepCount - 1);
+    for(int i = 0; i < stepCount; i++) {
+        values[i] = startValue + round(stepAmount * i);
+    }
+
+    return values;
+}
+
+RgbColor* Convert::ColorRangeToColors(RgbColor startColor, RgbColor endColor, int stepCount) {
+    int* values = ColorRangeToAnalogs(startColor, endColor, stepCount);
+    RgbColor* colors = new RgbColor[stepCount];
+
+    for(int i = 0; i < stepCount; i++) {
+        colors[i] = AnalogToColor(values[i]);
+    }
+
+    delete[] values;
 
     return colors;
 }
--- src/convert.h
+++ src/convert.h
@@ -7,7 +7,29 @@ class Convert {
          */
         static RgbColor AnalogToColor(int analogValue);
 
+        /**
+         * Convert a range of values and a step count to an array of RgbColor objects that
+         * transitions from the start value as a color through the end value as a color.
+         */
         static RgbColor* AnalogRangeToColors(int startValue, int endValue, int stepCount);
+
+        /**
+         * Convert an RgbColor object to an equivalent value ranging from 0 to 1023.
+         */
+        static int ColorToAnalog(RgbColor color);
+
+        /**
+         * Convert a range of RgbColor objects and a step count to an array of analog
+         * values ranging from 0 to 1023 that transitions in values from the start color
+         * to the end color.
+         */
+        static int* ColorRangeToAnalogs(RgbColor startColor, RgbColor endColor, int stepCount);
+
+        /**
+         * Convert a range of RgbColor objects and a step count to an array of RgbColor
+         * objects representing a color transition from start to end colors.
+         */
+        static RgbColor* ColorRangeToColors(RgbColor startColor, RgbColor endColor, int stepCount);
         
     private:
         // Disallow creating an instance of this object
